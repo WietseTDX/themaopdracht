c9bdc84ba3d50374b6cedeffcbb1d0f3b39fa420
Wietse <WietseTDam@gmail.com>
2019-11-01 13:30:55 +0100

    Maincontroller af niet getest

--- Maincontroller/MainController.cpp
+++ Maincontroller/MainController.cpp
@@ -1,70 +1,139 @@
 #include "Maincontroller.hpp"
 
+MainController::MainController(InputHandeler& handeler) : {
+	handeler.addButton(button);
+}
+
 void MainController::main() {
-  while (true) {
-    switch (state) {
-      case (states::WAIT_FOR_COMMAND) : {
-        auto event = wait(PeriodeFlag + CommandChannel);
-        if (event == PeriodeFlag) {
-					if (hwlib::now_us() >= last_us + 1000000) {
-						last_us += 1000000;
-						CommandData Time = CommandData(0, last_us/1000000);
-						oled.update(Time);
-						if (time <= 0) {
-							state = states::WAIT_FOR_PC;
-						}
-					}
-					if (event == CommandChannel) {
-						command = CommandChannel.read();
-						runCommand(commnad);
-					}
-      	}
-    	}
-			case (states::WAIT_FOR_PC): {
-				if (time <= 0) {
-					hwlib::cin >> keyboard;
-					if (keyboard == 111) {
-						hwlib::cout << player_information;
-						state = states::WAIT_FOR_START_GAME;
-					}
-				}
-			}
-			case (states::WAIT_FOR_START_GAME): {
+	while (true) {
+		switch (state) {
+			case (states::WAIT_FOR_STAR_GAME) : {
 				auto event = wait(CommandChannel);
-				if (CommandChannel.read().type == 4) {
+				if (startGame()) {
+					count_down = 10;
+					state = states::COUNTDOWN;
+				}
+			break;
+			}
+			case (states::COUNTDOWN) : {
+				auto event = wait(PeriodFlag);
+				count_down--;
+				if (count_down <= 0) {
 					state = states::WAIT_FOR_COMMAND;
 				}
+			break;
 			}
-  	}
+			case (states::WAIT_FOR_COMMAND) : {
+				auto event = wait(ShotTimer + BeenShotTimer + ButtonPressedFlag + BuzzerTimer + CommandChannel + PeriodFlag);
+				if (event == ShotTimer) {
+					shot = false;
+				}
+				if (event == BeenShotTimer) {
+					been_shot = false;
+				}
+				if (event == buttonPressedFlag) {
+					if (ButtonIDPool.read() == buttons::shoot_trigger && (!shot)) {
+						IrSend.sendMessage(Info.getPlayerNumber(), Info.getWeapon());
+						shot = true;
+						ShotTimer.set(timeWaitShot());
+						Buzzer.write(1);
+						BuzzerTimer.set(500000);
+					}
+				}
+				if (event == BuzzerTimer) {
+					Buzzer.write(0);
+				}
+				if (event == CommandChannel) {
+					command = CommandChannel.read();
+					runCommand(command);
+					if (Info.getHealthPoints() <= 0) {
+						state == states::WAIT_FOR_PC;
+					}
+				}
+				if (event == PeriodFlag) {
+					Info.setTime(Info.getTime() - 1);
+					Window.update(0);
+					if (Info.getTime <= 0) {
+						state = states::WAIT_FOR_PC;
+					}
+				}
+			break;
+			}
+			case (states::WAIT_FOR_PC) : {
+				hwlib::cin >> keyboard;
+				if (keybaord == 111) {
+					hwlib::cout << Info;
+					state = states::WAIT_FOR_START;
+				}
+			break;
+			}
+		}
 	}
 }
 
-void MainController::buttonPressed(Button b) {
-	ButtonIDPool.write(b);
+
+void MainController::buttonPressed(int button) {
+	ButtonIDPool.write(button);
 	ButtonPressedFlag.set();
 }
-
-void MainController::translateCommand(CommandData data) {
+void MainController::translateCommand(StructData data) {
 	CommandChannel.write(data);
 }
 
-void MainController::runCommand(CommandData command) {
-	if (command.type == 1){//change player number
-		player_information.setPlayerNumber(command.data);
-		oled.update(command);
-	} else if (CommandData.type == 2) { //change health
-		int weapon = command.data && 0x31;
-		int player = command.data && 0x31 << 5;
-		addHit(player, weapon);
-		int damage = calculateDamage(weapon);
-		int health = player_information.getHealth();
+
+//=====================//
+//   interne functies  //
+//=====================//
+int MainController::timeWaitShot() {
+	int weapon = info.getWeapon();
+	int wait = 500000;
+	wait +=500000 * weapon;
+}
+
+int MainController::calculateDamage(int data) {
+	return data * 1;
+}
+void MainController::runCommand(StructData command) {
+	if(CommandData.type == 2){ //change health
+	int data = command.data & 0x001F;
+	int player = (command.data >> 5) & 0x001F;
+	if (player != 0 && (!been_shot) {
+		Buzzer.write(1);
+		BuzzerTimer.set(1000000);
+		info.addHit(player, weapon);
+		BeenShotTimer.set(3000000);
+		been_shot = true;
+		int damage = calculateDamage(data);
+		int health = info.getHealthPoints();
 		health -= damage;
-		if (health <= 0) {health = 0;}
-		player_information.setHealth(health);
-		command.data = health;
-		oled.update(command);
-	} else if (command.type == 3) { //change weapon
-		player_information.setWeapon(command.data);
-		oled.update(command);
+		if (health <= 0){ health = 0;}
+		info.setHealthPoints(health);
+		Window.update(2);
+	} 
+  }else if(command.type == 3){ //change weapon
+    player_information.setWeapon(command.data);
+    Window.update(3);
+  }
+}
+
+bool MainController::startGame() {
+	command = CommandChannel.read();
+	if (Command.type == 2) { // change time or start game
+		int data = command.data & 0x001F;
+		int player = (command.data >> 5) & 0x001F;
+		if (player == 0) {
+			if (((data >> 5) & 0x01)) {
+				info.setTime(data & 0b000001111);
+				return false;
+			}
+			if (data == 0) {
+				return true;
+			}
+		}
 	}
+	else if(command.type == 1){//change player number
+		player_information.setPlayerNumber(command.data);
+		Window.update(1);
+	}
+  return false;
 }
--- Maincontroller/MainController.hpp
+++ Maincontroller/MainController.hpp
@@ -1,27 +1,47 @@
 #include "hwlib.hpp"
 #include "rtos.hpp"
 
-struct CommandData {
-    CommandData(int type, int data) : type(type), data(data) {}
-    int type = 0;
-    int data = 0;
+struct StructData {
+	StructData(int to_change, uint16_t data) : to_change(to_change), data(data) {};
+	int to_change;
+	uint16_t data;
 };
 
-
-
 class MainController : public rtos::task<> {
-    private:
-    rtos::flag ButtonPressedFlag.set();
-    rtos::pool<Button> ButtonIDPool;
-    rtos::channel<CommandData, 10> CommandChannel;
-    rtos::periode PeriodFlag;
+	rtos::flag ButtonPressedFlag;
+	rtos::pool<int> ButtonIDPool;
+	rtos::channel<3, StructData> CommandChannel; //5 Omdat de Receiver kan elke 40 ms een bericht ontvangen en je kan niet sneller dan 10x per seconde een kopje indrukken en van de keyboard controller krijgt die een bericht per seconde omdat sneller wel heel lastig is. De main heeft een deadline van 50 ms neem ik aan dat voor de 40 ms van de receiver neemt 2 plekken in en de button een halve plek dat kan niet dus 1 en voor de keyborad ook 1 dus 3 plekken
+	rtos::timer ShotTimer;
+	rtos::timer BeenShotTimer;
+	rtos::timer BuzzerTimer
 
-    eunm class states {WAIT_FOR_COMMAND, WAIT_FOR_START_GAME, WIAT_FOR_PC};
-    states state = states::WAIT_FOR_COMMAND;
+	hwlib::window w;
 
-    public:
-    void main() override;
-    void buttonPressed(Button b);
-    void translateCommand(CommandData data);
-    void runCommand(CommandData command);
+	hwlib::due::pin_out(hwlib::due::pins::d5) buzzer;
+	IrSendController IrSend;
+	PlayerInformation<100> Info;
+	WindowController Window(w, Info);
+	Button button;
+	int count_down;
+	int keyboard;
+	bool shot = false;
+	bool been_shot = false;
+
+	enum class states {WAIT_FOR_COMMAND, WAIT_FOR_START_GAME, WAIT_FOR_PC, COUNTDOWN};
+	states state = states::WAIT_FOR_START_GAME;
+	enum buttons {shoot_trigger = 0};
+	public:
+	MainController(InputHandeler& handeler);
+	void main() override;
+	void buttonPressed(int button);
+	void translateCommand(StructData data);
+
+	// interne functies
+	private:
+	int timeWaitShot();
+
+	int calculateDamage(int data);
+	void runCommand(StructData command);
+
+	bool startGame();
 }
