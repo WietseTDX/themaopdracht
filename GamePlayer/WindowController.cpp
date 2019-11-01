ba0e86d6da94508a4812060ff42644ac8e8f1b0f
Jochem van Weelde <jochem.vanweelde@student.hu.nl>
2019-11-01 13:52:59 +0100

    OLEDController updated to new WindowController

--- GamePlayer/WindowController.cpp
+++ GamePlayer/WindowController.cpp
@@ -1,5 +1,5 @@
-#include "OLEDController.hpp"
-
+#include "WindowController.hpp"
+#include "PlayerInformation.hpp"
 //========================================
 //OTHER FUNCTIONS
 //========================================
@@ -71,55 +71,40 @@ constexpr auto top_heart_values = lookup<42, hwlib::xy>(hearttop);
 constexpr auto bottom_heart_values = lookup<42, hwlib::xy>(heartbottom);
 
 //========================================
-//PRIVATE FUNCTIONS OLEDCONTROLLER
+//PRIVATE FUNCTIONS WindowController
 //========================================
 
-void OLEDController::drawBox(hwlib::window_part_t & window, int x, int y){
+void WindowController::drawBox(hwlib::window_part_t & window, int x, int y){
         hwlib::rectangle(hwlib::xy(0,0), hwlib::xy(x, y)).draw(window);
     }
 
 //========================================
-//PUBLIC FUNCTIONS OLEDCONTROLLER
+//PUBLIC FUNCTIONS WindowController
 //========================================
 
-void OLEDController::updateTime(int time){
-    OledPool.write(time);
-    updateTimeFlag.set();
-}
-
-void OLEDController::updateHealth(int health_points){
-    OledPool.write(health_points);
-    updateHealthFlag.set();
-}
-
-void OLEDController::updatePlayerNumber(int player_number){
-    OledPool.write(player_number);
-    updatePlayerFlag.set();
-}
-
-void OLEDController::updateWeapon(int weapon){
-    OledPool.write(weapon);
-    updateWeaponFlag.set();
+void WindowController::update(int window_number){
+    WindowChannel.write(window_number);
 }
 
 //========================================
-//MAIN FUNCTION OLEDCONTROLLER
+//MAIN FUNCTION WindowController
 //========================================
 
-void OLEDController::main() {
+void WindowController::main() {
     state = states::WAIT_FOR_CHANGE;
-    oled.clear();
+    window.clear();
     while(true){
         switch( state ){
             case states::WAIT_FOR_CHANGE:
             {
-                auto event_flag = wait(updateTimeFlag + updateHealthFlag + updatePlayerFlag + updateWeaponFlag);
-                if(event_flag == updateTimeFlag){
+                wait(WindowChannel);
+                auto window_update = WindowChannel.read();
+                if(window_update == 0){
                     state = states::UPDATE_TIME;
-                }else if(event_flag == updateHealthFlag){
-                    state = states::UPDATE_HEALTH;
-                }else if(event_flag == updatePlayerFlag){
+                }else if(window_update == 1){
                     state = states::UPDATE_PLAYER_INFORMATION;
+                }else if(window_update == 2){
+                    state = states::UPDATE_HEALTH;
                 }else{
                     state = states::UPDATE_WEAPON;
                 }
@@ -127,39 +112,39 @@ void OLEDController::main() {
             }//case wait for change
             case states::UPDATE_TIME:
             {
-                auto time = OledPool.read();
-                displayTime << "\f" << textdisplay[time/600] << textdisplay[time/60%10] << ':' << textdisplay[time%60/10] << textdisplay[time%60%10];
-                drawBox(timeWindow, 79,16);
-                timeWindow.flush();
+                auto time = info.getTime();
+                display_time << "\f" << textdisplay[time/600] << textdisplay[time/60%10] << ':' << textdisplay[time%60/10] << textdisplay[time%60%10];
+                drawBox(time_window, 79,16);
+                time_window.flush();
                 state = states::WAIT_FOR_CHANGE;
                 break;
             }//case update time
             case states::UPDATE_HEALTH:
             {
-                int health_points = OledPool.read();
-                displayHealth << "\f" << "\t0202" << textdisplay[health_points/100] << textdisplay[health_points%100/10] << textdisplay[health_points%10];
+                int health_points = info.getHealthPoints();
+                display_health << "\f" << "\t0202" << textdisplay[health_points/100] << textdisplay[health_points%100/10] << textdisplay[health_points%10];
                 for(int i = 0; i < 41; i++){
-                    hwlib::line(top_heart_values.get(i), top_heart_values.get(i+1)).draw(healthWindow);
-                    hwlib::line(bottom_heart_values.get(i), bottom_heart_values.get(i+1)).draw(healthWindow);
+                    hwlib::line(top_heart_values.get(i), top_heart_values.get(i+1)).draw(health_window);
+                    hwlib::line(bottom_heart_values.get(i), bottom_heart_values.get(i+1)).draw(health_window);
                 }
-                drawBox(healthWindow, 63, 44);
-                healthWindow.flush();
+                drawBox(health_window, 63, 44);
+                health_window.flush();
                 state = states::WAIT_FOR_CHANGE;
                 break;
             }//case update health
             case states::UPDATE_PLAYER_INFORMATION:
             {
-                int player_number = OledPool.read();
-                displayPlayer << "\f" << "P:" << textdisplay[player_number];
-                drawBox(playerNumberWindow, 46, 16);
-                playerNumberWindow.flush();
+                int player_number = info.getPlayerNumber();
+                display_player << "\f" << "P:" << textdisplay[player_number];
+                drawBox(player_number_window, 46, 16);
+                player_number_window.flush();
                 state = states::WAIT_FOR_CHANGE;
                 break;
             }//case update player information
             case states::UPDATE_WEAPON:
             {
-                drawBox(weaponWindow, 62, 44);
-                weaponWindow.flush();
+                drawBox(weapon_window, 62, 44);
+                weapon_window.flush();
                 state = states::WAIT_FOR_CHANGE;
                 break;
             }//case update weapon
--- GamePlayer/main.cpp
+++ GamePlayer/main.cpp
@@ -1,39 +1,48 @@
-#include "OLEDController.hpp"
-
+#include "WindowController.hpp"
+#include "PlayerInformation.hpp"
 
 class testMain : rtos::task<> {
 private:
-    OLEDController& oledcontroller;
+    PlayerInformation<100> & info;
+    hwlib::target::pin_oc scl                   = hwlib::target::pin_oc{ hwlib::target::pins::scl };
+    hwlib::target::pin_oc sda                   = hwlib::target::pin_oc{ hwlib::target::pins::sda };
+    hwlib::i2c_bus_bit_banged_scl_sda i2c_bus   = hwlib::i2c_bus_bit_banged_scl_sda( scl, sda );
+    hwlib::glcd_oled oled                       = hwlib::glcd_oled( i2c_bus );
+    WindowController oled_controller            = WindowController(oled, info);
 public:
 
-testMain(OLEDController & oledcontroller):
-    oledcontroller(oledcontroller)
-{}
+testMain(PlayerInformation<100> & info):
+    info(info)
+{
+
+}
 
 void main() override{
-    int time = 630;
+    oled_controller.update(0);
+    hwlib::wait_ms(5);
+    oled_controller.update(1);
+    hwlib::wait_ms(5);
+    oled_controller.update(2);
+    hwlib::wait_ms(5);
+    oled_controller.update(3);
+    hwlib::wait_ms(5);
     for(;;){
+        info.setTime(info.getTime()-1);
+        oled_controller.update(0);
         hwlib::wait_ms(1000);
-        oledcontroller.updateTime(time);
-        hwlib::wait_ms(1000);
-        oledcontroller.updatePlayerNumber(time%10);
-        hwlib::wait_ms(1000);
-        oledcontroller.updateHealth(time);
-        hwlib::wait_ms(1000);
-        oledcontroller.updateWeapon(time%100);
-        time--;
     }
 }
 };
 
 int main(){
     hwlib::wait_ms(2000);
-    auto scl             = hwlib::target::pin_oc{ hwlib::target::pins::scl };
-    auto sda             = hwlib::target::pin_oc{ hwlib::target::pins::sda };
-    auto i2c_bus         = hwlib::i2c_bus_bit_banged_scl_sda( scl, sda );
-    auto oled            = hwlib::glcd_oled( i2c_bus );
+    PlayerInformation<100> info;
+    info.setTime(640);
+    info.setHealthPoints(100);
+    info.setPlayerNumber(2);
+    info.setWeapon(4);
 
-    auto oledcontroller = OLEDController(oled);
-    auto gozer = testMain(oledcontroller);
+    auto main = testMain(info);
+    
     rtos::run();
 }
