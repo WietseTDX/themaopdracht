10e55c8eb1336d2ebc047e3400891697e2ee64fd
Wietse <WietseTDam@gmail.com>
2019-11-01 14:06:56 +0100

    Button handeler is af moet nog getest

---
+++ ButtonHandeler/ButtonHandeler.cpp
@@ -0,0 +1,17 @@
+#include "ButtonHandeler.hpp"
+
+void ButtonHandeler::main() {
+  while (true) {
+    auto event = wait(PeriodFlag);
+    keyboard.update();
+    for (int i=0; i<place; i++) {
+      button[i].update();
+    }
+  }
+}
+
+
+void ButtonHandeler::addButton(Button& object) {
+  button[place] = object;
+  place++;
+}
---
+++ ButtonHandeler/ButtonHandeler.hpp
@@ -0,0 +1,13 @@
+#include "hwlib.hpp"
+#include "rtos.hpp"
+
+template<int count_button>
+class ButthonHandeler : public rtos::task<> {
+  KeyboardControl &Keyboard;
+  Button *button[count_button];
+  int place = 0;
+
+  public:
+  void main() override;
+  void addButton(Button button);
+}
